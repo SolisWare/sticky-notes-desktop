0892a80a0736b968ac8d82c425651475928bdb08
Michael Polowczuk <michaelp@solisware.com>
2023-10-08 02:45:53 +0100

    Defined app color schema

---
+++ src/theme/AppColors.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2023 X-SiGMA Systems.
+ * 
+ * All rights reserved. Licensed under the MIT license.
+ * See the LICENSE.txt file in the project root directory for details.
+ */
+export const AppColors = {
+  // App's Primary color palette.
+  MAIN_LIGHT:      "#3CBBEB",
+  MAIN:            "#138EC7",
+  MAIN_DARK:       "#127DB3",
+  // App's Secondary color palette.
+  SECONDARY_LIGHT: "#52BE8F",
+  SECONDARY:        "#00A362",
+  SECONDARY_DARK: "#009557",
+  // App's accent color.
+  ACCENT: "#F0F7FA"
+}
