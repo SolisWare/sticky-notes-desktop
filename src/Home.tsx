24ea1d79539eb8fe62bb7fbf95fe65d85c41183c
Michael Polowczuk <michaelp@solisware.com>
2023-10-08 04:53:14 +0100

    Initialized Home component with basic MUI style

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,36 @@
+/**
+ * Copyright (c) 2023 X-SiGMA Systems.
+ * 
+ * All rights reserved. Licensed under the MIT license.
+ * See the LICENSE.txt file in the project root directory for details.
+ */
+import { Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { AppColors } from "./theme/AppColors";
+
 type HomeProps = {
   
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    minWidth: "100%",
+    minHeight: "100%",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    zIndex: 1,
+    backgroundColor: AppColors.ACCENT
+  }
+}));
+
 function Home(props: HomeProps) {
+  const classes = useStyles();
+  
   return (
-    <p>Home!</p>
+    <div className={classes.root}>
+      <p>Home</p>
+    </div>
   );
 }
 
