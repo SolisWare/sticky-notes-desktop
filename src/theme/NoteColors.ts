923308fcba837d88b73274dc30dcb23d90330ccf
Michael Polowczuk <michaelp@solisware.com>
2023-10-10 18:57:48 +0100

    Defined A Note backrgound colors

---
+++ src/theme/NoteColors.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2023 X-SiGMA Systems.
+ * 
+ * All rights reserved. Licensed under the MIT license.
+ * See the LICENSE.txt file in the project root directory for details.
+ */
+ export const NoteColors = {
+   YELLOW:  "#F7F0AD",
+   GREEN:   "#D0E6BB",
+   INDIGO:  "#D5DEFD",
+   RED:     "#FDE2E2",
+   TEAL:    "#C9EFE6",
+   LILAC:   "#E7CCF1"
+ }
+ 
