c248581a1baeb5bd141203785784b64164ed535e
Michael Polowczuk <michaelp@solisware.com>
2023-10-21 13:05:04 +0100

    Added toolbar buttton

--- src/components/XToolbar.tsx
+++ src/components/XToolbar.tsx
@@ -4,22 +4,66 @@
  * All rights reserved. Licensed under the MIT license.
  * See the LICENSE.txt file in the project root directory for details.
  */
+import { makeStyles } from "@mui/styles";
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import Toolbar from '@mui/material/Toolbar';
+import { Box, Button, Theme } from "@mui/material";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { AppTheme } from "../theme/AppTheme";
+
+const appTheme = AppTheme.Theme;
 
 type XToolbarProps = {
   title: string;
 };
 
+const useStyles = makeStyles((theme: Theme) => ({
+  toolbar: {
+    height: 20
+  },
+  toolbarBtnsContainer: {
+    padding: "5px",
+    marginLeft: "25px"
+  },
+  toolbarBtn: {
+    width: 125,
+    height: 32
+  },
+  toolbarIconBtnInnerContainer: {
+    display: "flex",
+    alignItems: "center",
+    verticalAlign: "center"
+  },
+  toolbarBtnText: {
+    paddingTop: "3px",
+    paddingLeft: "5px"
+  }
+}));
+
+const handleAddNote = () => {
+
+}
+
 function XToolbar(props: XToolbarProps) {
+  const classes = useStyles();
+  
   return (
     <AppBar position="static">
-      <Toolbar variant="dense">
+      <Toolbar className={classes.toolbar}>
         {/* TODO: Add app icon. */}
-        <Typography fontWeight="bold">
+        <Typography variant="h6" fontWeight="bold">
           {props.title}
         </Typography>
+        <Box className={classes.toolbarBtnsContainer}>
+          <Button className={classes.toolbarBtn} variant="toolbar" color="primary" onClick={() => handleAddNote()}>
+            <div className={classes.toolbarIconBtnInnerContainer}>
+              <AddCircleOutlineIcon fontSize="small" />
+              <Typography className={classes.toolbarBtnText} variant="body2">New Note</Typography>
+            </div>
+          </Button>
+          
+        </Box>
       </Toolbar>
     </AppBar>
   );
