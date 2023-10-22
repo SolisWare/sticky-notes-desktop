2c8ae2a8d4940841404ffb2fe863323367896049
Michael Polowczuk <michaelp@solisware.com>
2023-10-22 03:58:33 +0100

    Implemented a custom textarea component to store a note main content

---
+++ src/components/XTextarea.tsx
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2023 X-SiGMA Systems.
+ * 
+ * All rights reserved. Licensed under the MIT license.
+ * See the LICENSE.txt file in the project root directory for details.
+ */
+import { Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+ 
+type XTextareaProps = {
+  placeholder: string;
+  content: string;
+}
+ 
+const useStyles = makeStyles((theme: Theme) => ({
+  xTextarea: {
+    width: "100%",
+    height: "100%",
+    minHeight: "100%",
+    background: "transparent",
+    outline: "none",
+    border: "none",
+    resize: "none"
+  }
+}));
+ 
+function XTextarea(props: XTextareaProps) {
+  const classes = useStyles();
+   
+  return (
+    <textarea className={classes.xTextarea}
+              placeholder={props.placeholder}
+              defaultValue={props.content} />
+  );
+}
+ 
+export default XTextarea;
+ 
