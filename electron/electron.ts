ca4ac7fb64e4823622cbb96ef60d530603497bc8
Michael Polowczuk <michaelp@solisware.com>
2024-09-16 01:54:48 +0100

    electron.js refactoring and adjustment for multi window application

--- electron/electron.ts
+++ electron/electron.ts
@@ -5,52 +5,63 @@
  * See the LICENSE.txt file in the project root directory for details.
  */
 import { Platform } from './../src/utils/Platform';
-import { app, BrowserWindow, Menu } from "electron";
+import { app, BrowserWindow, LoadFileOptions, Menu } from "electron";
 import * as path from "path";
 import { createFileRoute, createURLRoute } from 'electron-router-dom'
 import menubar from "./menu";
 import * as isDev from "electron-is-dev"
 
-function createWindow() {
-  
+// Load index.html as the app entry point for production
+// and listen on "http://localhost:3000" in 'dev' mode.
+const dev = (handle: string): string => {
+  return createURLRoute(
+    "http://localhost:3000",
+    handle
+  ).toString();
+};
+
+const production = (handle: string): [string, LoadFileOptions] => {
+  return createFileRoute(
+    path.join(__dirname, "/../index.html"),
+    handle
+  );
+};
+
+const createMainWindow = () => {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 1235,
     height: 800,
     minWidth: 335,
-    minHeight:250,
+    minHeight: 250,
     webPreferences: {
+      webSecurity: false,
       nodeIntegration: true,
-      contextIsolation: false,
+      contextIsolation: true,
       preload: path.join(__dirname, "preload.js")
-      
-    },
+
+    }
   });
   
-  // Load index.html as the app entry point for production
-  // and listen on "http://localhost:3000" in 'dev' mode.
-  const devConfig = createURLRoute("http://localhost:3000", 'main');
-  const productionConfig = createFileRoute(path.join(__dirname, "index.html"), 'main');
-  
   if (isDev) {
-    win.loadURL(devConfig);
-  } else {
-    win.loadFile(...productionConfig);
-  }
-  
-  // Open DevTools in 'dev' mode only
-  if (isDev) {
+    // Load the URL for the main window
+    win.loadURL(dev("main"));
+    // Open DevTools when running in dev mode
     win.webContents.openDevTools({ mode: "detach" });
+  } else {
+    win.loadFile(...production("main"));
   }
-  
-  // Load the menubar items
-  Menu.setApplicationMenu(menubar);
-}
+};
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+// Load the menubar items
+Menu.setApplicationMenu(menubar);
+
+// This method is called when Electron has finished the initialization
+// and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createMainWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -61,16 +72,15 @@ app.on('window-all-closed', () => {
   }
 });
 
-// On macOS it's common to re-create a window in the app when the
-// dock icon is clicked and there are no other windows open.
+// On macOS it's common to re-create a window in the app 
+// when the dock icon is clicked and there are no other windows opened.
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createMainWindow();
   }
 });
 
 // "About" dialog window customization
-// TODO: Move to a dedicated file and add more information.
 app.setAboutPanelOptions({
   applicationName: "X-NoTES",
   applicationVersion: "0.1.0",
@@ -78,5 +88,5 @@ app.setAboutPanelOptions({
   authors: [
     "X-SiGMA Systems"
   ],
-  copyright: "Copyright © 2023 X-SiGMA Systems.\nAll rights reserved."
+  copyright: "Copyright © 2023-2024 X-SiGMA Systems.\nAll rights reserved."
 });
