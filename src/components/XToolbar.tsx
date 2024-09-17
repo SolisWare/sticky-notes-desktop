4f194a05e49648ea5190340120c57aa8656f05c2
Michael Polowczuk <michaelp@solisware.com>
2024-09-17 22:54:42 +0100

    Updated copyrights

--- src/components/XToolbar.tsx
+++ src/components/XToolbar.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 X-SiGMA Systems.
+ * Copyright (c) 2023-2024 X-SiGMA Systems.
  * 
  * All rights reserved. Licensed under the MIT license.
  * See the LICENSE.txt file in the project root directory for details.
