4c4524ec7e68b29142b76d0370877ef6a33b2117
Michael Polowczuk <michaelp@solisware.com>
2026-04-25 17:05:35 +0100

    Color schema fixes for the ConfirmationDialog and prevent it from being dismissed on backdropClick

--- src/components/ConfirmationDialog.tsx
+++ src/components/ConfirmationDialog.tsx
@@ -4,7 +4,7 @@
  * All rights reserved. Licensed under the MIT license.
  * See the LICENSE.txt file in the project root directory for details.
  */
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Theme } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogProps, DialogTitle, Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { getAppColors } from "../theme/AppColors";
 import { SystemTheme } from "../theme/SystemTheme";
@@ -21,9 +21,33 @@ type ConfirmationDialogProps = {
   onCancel: () => void;
 };
 
-const useStyles = makeStyles<Theme, AppColorStyleProps>((theme: Theme) => ({
+enum DialogCloseReason {
+  backdropClick = "backdropClick",
+  escapeKeyDown = "escapeKeyDown"
+}
+
+const useStyles = makeStyles<Theme, AppColorStyleProps>((_theme: Theme) => ({
+  backdrop: {
+    backgroundColor: ({ appColors }) => appColors.DIALOG_BACKDROP + " !important",
+  },
+  dialogPaper: {
+    backgroundColor: ({ appColors }) => appColors.DIALOG_BACKGROUND,
+  },
   title: {
-    color: ({ appColors }) => appColors.MAIN,
+    backgroundColor: ({ appColors }) => appColors.DIALOG_BACKGROUND,
+    color: ({ appColors }) => appColors.DIALOG_TITLE_TEXT,
+  },
+  content: {
+    backgroundColor: ({ appColors }) => appColors.DIALOG_BACKGROUND,
+  },
+  actions: {
+    backgroundColor: ({ appColors }) => appColors.DIALOG_BACKGROUND,
+  },
+  message: {
+    color: ({ appColors }) => appColors.DIALOG_TEXT + " !important",
+  },
+  cancelButton: {
+    color: ({ appColors }) => appColors.DIALOG_CANCEL_TEXT + " !important",
   },
 }));
 
@@ -31,14 +55,26 @@ function ConfirmationDialog(props: ConfirmationDialogProps) {
   const appColors = getAppColors(props.theme);
   const classes = useStyles({ appColors });
 
+  const handleClose: DialogProps["onClose"] = (_event, reason) => {
+    if (reason === DialogCloseReason.backdropClick) {
+      return;
+    }
+    props.onCancel();
+  };
+
   return(
-    <Dialog open={props.open} onClose={props.onCancel}>
+    <Dialog
+      open={props.open}
+      onClose={handleClose}
+      classes={{ paper: classes.dialogPaper }}
+      BackdropProps={{ classes: { root: classes.backdrop } }}
+    >
       <DialogTitle className={classes.title}>{props.title}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>{props.message}</DialogContentText>
+      <DialogContent className={classes.content}>
+        <DialogContentText className={classes.message}>{props.message}</DialogContentText>
       </DialogContent>
-      <DialogActions>
-        <Button onClick={props.onCancel}>
+      <DialogActions className={classes.actions}>
+        <Button className={classes.cancelButton} onClick={props.onCancel}>
           {props.cancelLabel ?? "Cancel"}
         </Button>
         <Button onClick={props.onConfirm} color="error" variant="contained">
